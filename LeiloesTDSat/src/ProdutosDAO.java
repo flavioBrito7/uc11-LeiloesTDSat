36bbad3d3eb37f38f0c7f127f240a41dfa5bfcc4
Flavio Brito <flavioandersondebrito@gmail.com>
2024-12-06 15:40:18 -0300

    Commit #3 - Alterando a classe ProdutosDAO para cadastrar no banco de dados

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -12,7 +12,9 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
 
 
 public class ProdutosDAO {
@@ -22,12 +24,24 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+     public void cadastrarProduto (ProdutosDTO produto){
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try {           
+                conn = new conectaDAO().connectDB();
+                String sql = "INSERT INTO produtos (nome, valor, status) VALUES (  ?, ?, ? )";
+
+                prep = conn.prepareStatement(sql);
+                prep.setString(1, produto.getNome() );
+                prep.setInt(2, produto.getValor() );
+                prep.setString(3, produto.getStatus() );
+
+                prep.execute();
+                prep.close();
+                
+        } catch (SQLException e) {
+                e.printStackTrace();
+        }
+                 
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
