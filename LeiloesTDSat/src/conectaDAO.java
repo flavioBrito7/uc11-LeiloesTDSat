ea5caa596489f977397ef61d4b095ef159523cbe
Flavio Brito <flavioandersondebrito@gmail.com>
2024-12-06 15:20:11 -0300

    Commit #1 - Definindo a tela principal: cadastroVIEW

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -22,8 +22,8 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=admin&password=");
-         
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
